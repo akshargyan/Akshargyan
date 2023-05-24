fa9bf593cd973ccf092f7b9b4bcafbf91939df57
akshargyan <98343605+akshargyan@users.noreply.github.com>
2023-05-24 22:45:42 +0530

    2nd year addition

--- team.js
+++ team.js
@@ -40,7 +40,7 @@ new CardShow(
   'team/2nd_year/Yash Gupta.jpg',
   1
 );
-// 3rd years
+
 new CardShow(
   'Abhinav Pal',
   'Mechanical Engineering',
@@ -51,9 +51,70 @@ new CardShow(
   1
 );
 // 2nd years
+
+
+// Sharad Tomar
+new CardShow(
+  'Sharad Tomar',
+  'Electronics and Communications Engineering',
+  'https://www.instagram.com/sharadrajputt/',
+  'https://www.linkedin.com/in/sharadtomarr',
+  'https://twitter.com/Sharadrajputt?t=uXOwezKVGvbALKdliz1gYw&s=08',
+  'team/1st_year/Sharad Tomar.jpeg',
+  2
+);
+// Rohit Rana
+new CardShow(
+  'Rohit Rana',
+  'Mechanical Engineering',
+  'https://instagram.com/rohitrana_17?igshid=MzNlNGNkZWQ4Mg==',
+  '#',
+  '#',
+  'team/1st_year/IMG_20230421_155455 - Rohit Rana.jpg',
+  2
+);
+
+// Nitin Kumar Rajpoot
+
+new CardShow(
+  'Nitin Kumar Rajpoot',
+  'Civil Engineering',
+  'https://www.instagram.com/nikki____don/',
+  'https://www.linkedin.com/in/nitin-kumar-rajpoot-10551219b',
+  'https://twitter.com/NitinKumarNikk4?t=Gl8ZdrbaYgZ1KXXIiK8cyg&s=08',
+  'team/1st_year/Nitin Kumar Rajpoot.jpg',
+  2
+);
+
+// Aditya Verma
+new CardShow(
+'Aditya Verma',
+'Electronics and Communications Engineering',
+'https://www.instagram.com/aditya.v12345/',
+'#',
+'#',
+'team/1st_year/Aditya Verma-min.jpg',
+2
+);
+
+// Anshul Mittal
+
+new CardShow(
+  'Anshul Mittal',
+  'Information Technology',
+  'https://www.instagram.com/mittalanshul_1012/',
+  'https://www.linkedin.com/in/anshul-mittal-14a041238/',
+  'https://twitter.com/amittal1012',
+  'team/1st_year/Anshul.jpeg',
+  2
+);
+
+
+
+
 // Lekhansh Sachan
 new CardShow(
-  'Lekhansh Sachan ',
+  'Lekhansh Sachan',
   'Electronics and Communications Engineering',
   'https://www.instagram.com/invites/contact/?i=1ercys0vpmbw9&utm_content=wmatqp',
   'https://www.linkedin.com/in/lekhansh-sachan-6079a523a',
@@ -61,3 +122,40 @@ new CardShow(
   'team/1st_year/Lekhansh Sachan .jpg',
   2
 );
+
+// Pratham Gupta 
+
+new CardShow(
+  'Pratham Gupta ',
+  'Civil Engineering',
+  'https://instagram.com/pratham_g_8633?igshid=MzNlNGNkZWQ4Mg==',
+  '#',
+  '#',
+  'team/1st_year/IMG_20230509_163723 - Pratham Gupta.jpg',
+  2
+)
+
+// Yashswani Mishra
+
+new CardShow(
+  'Yashswani Mishra',
+  'Information Technology',
+  'https://www.instagram.com/yashu.xo/',
+  'https://mobile.twitter.com/YashswaniMishra',
+  'https://www.linkedin.com/in/yashswani-mishra-she-her-788803227/',
+  'team/1st_year/Yashswani Mishra.jpeg',
+  2
+
+);
+
+// Anammya Vaish 
+
+new CardShow(
+  'Anammya Vaish ',
+  'Electronics and Communications Engineering',
+  'https://www.instagram.com/anammyavaish/',
+  'www.linkedin.com/in/anammya-vaish-854681223',
+  '#',
+  'team/1st_year/Anammya Vaish.jpeg',
+  2
+)
