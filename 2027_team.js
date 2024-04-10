a6faaf69e418e6ad14a5652b0d5da1787d4402e5
Aryam Gupta <aryamgupta4@gmail.comgit config --global user.name Aryamgupta--global to set the identity only in this respositorygit remote add origin github.com/Aryamgupta/2000910100033.git>
2024-04-11 00:57:10 +0530

    members arrangement updated

--- 2027_team.js
+++ 2027_team.js
@@ -4,89 +4,6 @@
 
 const data = [
   {
-    nameID: "BodhRatan",
-    email: "bodhratan1211@gmail.com",
-    name: "Bodh Ratan ",
-    branch: "Computer Science Engineering (AI & ML)",
-    instagram_Link: "https://www.instagram.com/_bodh._?igsh=cWVlZXRqNTYxbDdh",
-    twitterLink: "#",
-    linkedin_Link:
-      "https://www.linkedin.com/in/bodh-ratan-750a592b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink: "team/2023-27/bodhRatan.jpg",
-  },
-  {
-    nameID: "HarshKumarSingh",
-    email: "hkumarsingh137@gmail.com",
-    name: "Harsh Kumar Singh ",
-    branch: "Electrical and Electronics Engineering",
-    instagram_Link:
-      "https://www.instagram.com/harshkumar6383?utm_source=qr&igsh=aW12ZXIzcXgxNzU5",
-    twitterLink: "https://x.com/HarshKu77903621?t=MjtixHbDeVK_tfALGQnHmQ&s=08",
-    linkedin_Link:
-      "https://www.linkedin.com/in/harsh-kumar-singh-684831300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink:
-      "team/2023-27/Screenshot_2024-04-01-12-34-34-45_92460851df6f172a4592fca41cc2d2e6 - Harsh Kumar Singh.jpg",
-  },
-  {
-    nameID: "IshaGupta",
-    email: "gisha825@gmail.com",
-    name: "Isha Gupta ",
-    branch: "Computer Science Engineering (AI & ML)",
-    instagram_Link:
-      "https://www.instagram.com/its_isha.25?igsh=dnk0azF6amhqaXA4",
-    linkedin_Link:
-      "https://www.linkedin.com/in/isha-gupta-b6a5232a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    twitterLink: "https://x.com/isha_gupta2507?t=GjH6x1w4VtDzFTZVwyKt1g&s=08",
-    imageLink: "team/2023-27/IMG-20240401-WA0004 - Isha Gupta.jpg",
-  },
-  {
-    nameID: "Ishanpandey",
-    email: "ishupandey0301@gmail.com",
-    name: "Ishan pandey",
-    branch: "Computer Science Engineering (AI & ML)",
-    instagram_Link:
-      "https://www.instagram.com/_ishu.pandit?igsh=MXF2dmgwdThkcXk1Mg==",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/ishan-pandey.jpg",
-  },
-  {
-    nameID: "Ishitasoni",
-    email: "cseishitasoni@gmail.com",
-    name: "Ishita soni",
-    branch: "Computer Science Engineering",
-    instagram_Link: "#",
-    twitterLink: "#",
-    linkedin_Link:
-      "https://www.linkedin.com/in/ishita-soni-27052a216/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink: "team/2023-27/Snapchat-2128963328 - Ishita soni.jpg",
-  },
-  {
-    nameID: "KaushalendraSingh",
-    email: "kingsingh1707@gmail.com",
-    name: "Kaushalendra Singh",
-    branch: "Information and Technology",
-    instagram_Link:
-      "https://www.instagram.com/kaushhalendra_247?igsh=ZGUzMzM3NWJiOQ==",
-    twitterLink: "https://x.com/kaushalendra03?t=I4ZnTBLIpvtT4VM14XedvQ&s=09",
-    linkedin_Link:
-      "https://www.linkedin.com/in/kaushalendrasingh03?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink:
-      "team/2023-27/kaushhalendra_247-20240406-0001 - 23IT015_Kaushalendra Singh.jpg",
-  },
-  {
-    nameID: "KhushiTiwari",
-    email: "khushi.drdz@gmail.com",
-    name: "Khushi Tiwari",
-    branch: "Computer Science Engineering (AI & ML)",
-    instagram_Link:
-      "https://www.instagram.com/_kiziee.dz_?igsh=cXQ2eWNmNnozYmk1",
-    twitterLink: "#",
-    linkedin_Link:
-      "https://www.linkedin.com/in/khushi-tiwari-5012412a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink: "team/2023-27/20240407_180424-1 - Pramod Kumar Tiwari.jpg",
-  },
-  {
     nameID: "ManeeshSingh",
     email: "singhmaneesh1492@gmail.com",
     name: "Maneesh Singh",
@@ -96,95 +13,6 @@ const data = [
     twitterLink: "#",
     linkedin_Link: "#",
     imageLink: "team/2023-27/maneesh.png",
-  },
-  {
-    nameID: "NaziyaParveen",
-    email: "naziyap192@gmail.com",
-    name: "Naziya Parveen ",
-    branch: "Computer Science Engineering (AI & ML)",
-    instagram_Link:
-      "https://www.instagram.com/naziyafaiz192?igsh=MW82dDF4Z3o5dG9taA==",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/IMG20231207123707 - Naziya Parveen.jpg",
-  },
-  {
-    nameID: "Nidhiyadav",
-    email: "ny32877@gmail.com",
-    name: "Nidhi yadav ",
-    branch: "Electronics and Communications Engineering",
-    instagram_Link: "https://www.instagram.com/nidhiyadav._ny/",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/nidhi.jpg",
-  },
-  {
-    nameID: "ParthAryan",
-    email: "pstar4268@gmail.com",
-    name: "Parth Aryan ",
-    branch: "Information and Technology",
-    instagram_Link: "Instagram.com/nearly.parth",
-    twitterLink: "https://x.com/DarthVa91208007?t=8X-F9773p8UJWFC0-aJFHg&s=09",
-    linkedin_Link:
-      "https://www.linkedin.com/in/parth-aryan-3b8753295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink: "team/2023-27/InShot_20240326_234551814 - PARTH ARYAN.jpg",
-  },
-  {
-    nameID: "PrinceSingh",
-    email: "sngprince21@gmail.com",
-    name: "Prince Singh ",
-    branch: "Information and Technology",
-    instagram_Link:
-      "https://www.instagram.com/princesingh_021?igsh=anZ3MHJ5azJ1dzll",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/prince.jpg",
-  },
-  {
-    nameID: "PriyanshiTripathi",
-    email: "tripathipriyanshi112@gmail.com",
-    name: "Priyanshi Tripathi ",
-    branch: "Electronics and Communications Engineering",
-    instagram_Link:
-      "https://www.instagram.com/priyanshitripathi513?igsh=bWtqZXBqNTR4ZGRh",
-    twitterLink: "https://x.com/PriyanshiT69118?t=OQ3nvR33Qc71rXIs07_Xog&s=09",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/photograph - Priyanshi Tripathi.jpg",
-  },
-  {
-    nameID: "RishitaSoni",
-    email: "rishitasonigdgps@gmail.com",
-    name: "Rishita Soni ",
-    branch: "Computer Science Engineering",
-    instagram_Link:
-      "https://www.instagram.com/rishitasoni05?igsh=MXMzNWNta3J4eWt3dA==",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink:
-      "team/2023-27/IMG-20240401-WA0014 - 23CSE138_RISHITA SONI(1).jpg",
-  },
-  {
-    nameID: "RiteshGupta",
-    email: "rg4999928@gmail.com",
-    name: "Ritesh Gupta",
-    branch: "Computer Science Engineering",
-    instagram_Link:
-      "https://www.instagram.com/ritesh.1724_?igsh=ODRtMTNuMTFiOTNv",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink:
-      "team/2023-27/IMG_20231022_114712_024 - Ritesh Gupta_23CSE104.webp",
-  },
-  {
-    nameID: "SatyamGupta",
-    email: "satyamstark7@gmail.com",
-    name: "Satyam Gupta",
-    branch: "Computer Science Engineering",
-    instagram_Link:
-      "https://www.instagram.com/satyam._.gupta._?igsh=MWZhdXhidnk3cmFqcw%3D%3D&utm_source=qr",
-    twitterLink: "#",
-    linkedin_Link: "https://in.linkedin.com/in/satyam-gupta-2b10742a4",
-    imageLink: "team/2023-27/IMG_0097 - Satyam Gupta.jpeg",
   },
   {
     nameID: "SaumyaSinghParihar",
@@ -199,6 +27,63 @@ const data = [
       "team/2023-27/Kaagaz_20240407_113021098497-1 (1) - 23IT019_ Saumya Singh Parihar.jpg",
   },
   {
+    nameID: "PriyanshiTripathi",
+    email: "tripathipriyanshi112@gmail.com",
+    name: "Priyanshi Tripathi ",
+    branch: "Electronics and Communications Engineering",
+    instagram_Link:
+      "https://www.instagram.com/priyanshitripathi513?igsh=bWtqZXBqNTR4ZGRh",
+    twitterLink: "https://x.com/PriyanshiT69118?t=OQ3nvR33Qc71rXIs07_Xog&s=09",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/photograph - Priyanshi Tripathi.jpg",
+  },
+
+  {
+    nameID: "RishitaSoni",
+    email: "rishitasonigdgps@gmail.com",
+    name: "Rishita Soni ",
+    branch: "Computer Science Engineering",
+    instagram_Link:
+      "https://www.instagram.com/rishitasoni05?igsh=MXMzNWNta3J4eWt3dA==",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink:
+      "team/2023-27/IMG-20240401-WA0014 - 23CSE138_RISHITA SONI(1).jpg",
+  },
+  {
+    nameID: "Ishitasoni",
+    email: "cseishitasoni@gmail.com",
+    name: "Ishita soni",
+    branch: "Computer Science Engineering",
+    instagram_Link: "#",
+    twitterLink: "#",
+    linkedin_Link:
+      "https://www.linkedin.com/in/ishita-soni-27052a216/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink: "team/2023-27/Snapchat-2128963328 - Ishita soni.jpg",
+  },
+  {
+    nameID: "Ishanpandey",
+    email: "ishupandey0301@gmail.com",
+    name: "Ishan pandey",
+    branch: "Computer Science Engineering (AI & ML)",
+    instagram_Link:
+      "https://www.instagram.com/_ishu.pandit?igsh=MXF2dmgwdThkcXk1Mg==",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/ishan-pandey.jpg",
+  },
+  {
+    nameID: "NaziyaParveen",
+    email: "naziyap192@gmail.com",
+    name: "Naziya Parveen ",
+    branch: "Computer Science Engineering (AI & ML)",
+    instagram_Link:
+      "https://www.instagram.com/naziyafaiz192?igsh=MW82dDF4Z3o5dG9taA==",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/IMG20231207123707 - Naziya Parveen.jpg",
+  },
+  {
     nameID: "SaurabhSingh",
     email: "singhsaurabh231217@gmail.com",
     name: "Saurabh Singh ",
@@ -207,38 +92,6 @@ const data = [
     twitterLink: "#",
     linkedin_Link: "#",
     imageLink: "team/2023-27/IMG_20240305_190202 - Saurabh Singh.jpg",
-  },
-  {
-    nameID: "SejalGupta",
-    email: "sejalgupta.9816@gmail.com",
-    name: "Sejal Gupta",
-    branch: "Electrical Engineering",
-    instagram_Link: "https://www.instagram.com/sejalgupta941/",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/sejal-gupta.jpg",
-  },
-  {
-    nameID: "SudikshaShukla",
-    email: "sudikshashukla24@gmail.com",
-    name: "Sudiksha Shukla ",
-    branch: "Electrical and Electronics Engineering",
-    instagram_Link: "https://www.instagram.com/sudiksh.a?igsh=enAzbXEwY2N2Y2x5",
-    twitterLink: "https://x.com/sudiksha_shukla?t=3Z0QV0SG0Bp2BXzOW5wl3g&s=08",
-    linkedin_Link:
-      "https://www.linkedin.com/in/sudiksha-shukla-b89818299?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    imageLink: "team/2023-27/IMG_20230508_202952_0045 - Sudiksha Shukla(1).jpg",
-  },
-  {
-    nameID: "Swayamkushwaha",
-    email: "swayamkushwaha@2006gmail.com",
-    name: "Swayam kushwaha",
-    branch: "Information and Technology",
-    instagram_Link:
-      "https://www.instagram.com/_swayam.k_?igsh=ZmExMGN2bTlicHpo",
-    twitterLink: "#",
-    linkedin_Link: "#",
-    imageLink: "team/2023-27/IMG-20220815-WA0001 - Swayam Kushwaha.jpg",
   },
   {
     nameID: "UtkarshRana",
@@ -272,7 +125,145 @@ const data = [
     linkedin_Link: "#",
     imageLink: "team/2023-27/IMG_20220423_134238-01-01 - Vishal Yadav.jpeg",
   },
+  {
+    nameID: "IshaGupta",
+    email: "gisha825@gmail.com",
+    name: "Isha Gupta ",
+    branch: "Computer Science Engineering (AI & ML)",
+    instagram_Link:
+      "https://www.instagram.com/its_isha.25?igsh=dnk0azF6amhqaXA4",
+    linkedin_Link:
+      "https://www.linkedin.com/in/isha-gupta-b6a5232a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterLink: "https://x.com/isha_gupta2507?t=GjH6x1w4VtDzFTZVwyKt1g&s=08",
+    imageLink: "team/2023-27/IMG-20240401-WA0004 - Isha Gupta.jpg",
+  },
+  {
+    nameID: "ParthAryan",
+    email: "pstar4268@gmail.com",
+    name: "Parth Aryan ",
+    branch: "Information and Technology",
+    instagram_Link: "Instagram.com/nearly.parth",
+    twitterLink: "https://x.com/DarthVa91208007?t=8X-F9773p8UJWFC0-aJFHg&s=09",
+    linkedin_Link:
+      "https://www.linkedin.com/in/parth-aryan-3b8753295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink: "team/2023-27/InShot_20240326_234551814 - PARTH ARYAN.jpg",
+  },
+  {
+    nameID: "RiteshGupta",
+    email: "rg4999928@gmail.com",
+    name: "Ritesh Gupta",
+    branch: "Computer Science Engineering",
+    instagram_Link:
+      "https://www.instagram.com/ritesh.1724_?igsh=ODRtMTNuMTFiOTNv",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink:
+      "team/2023-27/IMG_20231022_114712_024 - Ritesh Gupta_23CSE104.webp",
+  },
+  {
+    nameID: "SejalGupta",
+    email: "sejalgupta.9816@gmail.com",
+    name: "Sejal Gupta",
+    branch: "Electrical Engineering",
+    instagram_Link: "https://www.instagram.com/sejalgupta941/",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/sejal-gupta.jpg",
+  },
+  {
+    nameID: "PrinceSingh",
+    email: "sngprince21@gmail.com",
+    name: "Prince Singh ",
+    branch: "Information and Technology",
+    instagram_Link:
+      "https://www.instagram.com/princesingh_021?igsh=anZ3MHJ5azJ1dzll",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/prince.jpg",
+  },
+  {
+    nameID: "SudikshaShukla",
+    email: "sudikshashukla24@gmail.com",
+    name: "Sudiksha Shukla ",
+    branch: "Electrical and Electronics Engineering",
+    instagram_Link: "https://www.instagram.com/sudiksh.a?igsh=enAzbXEwY2N2Y2x5",
+    twitterLink: "https://x.com/sudiksha_shukla?t=3Z0QV0SG0Bp2BXzOW5wl3g&s=08",
+    linkedin_Link:
+      "https://www.linkedin.com/in/sudiksha-shukla-b89818299?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink: "team/2023-27/IMG_20230508_202952_0045 - Sudiksha Shukla(1).jpg",
+  },
+  {
+    nameID: "Nidhiyadav",
+    email: "ny32877@gmail.com",
+    name: "Nidhi yadav ",
+    branch: "Electronics and Communications Engineering",
+    instagram_Link: "https://www.instagram.com/nidhiyadav._ny/",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/nidhi.jpg",
+  },
+  {
+    nameID: "Swayamkushwaha",
+    email: "swayamkushwaha@2006gmail.com",
+    name: "Swayam kushwaha",
+    branch: "Information and Technology",
+    instagram_Link:
+      "https://www.instagram.com/_swayam.k_?igsh=ZmExMGN2bTlicHpo",
+    twitterLink: "#",
+    linkedin_Link: "#",
+    imageLink: "team/2023-27/IMG-20220815-WA0001 - Swayam Kushwaha.jpg",
+  },
 
+  {
+    nameID: "KaushalendraSingh",
+    email: "kingsingh1707@gmail.com",
+    name: "Kaushalendra Singh",
+    branch: "Information and Technology",
+    instagram_Link:
+      "https://www.instagram.com/kaushhalendra_247?igsh=ZGUzMzM3NWJiOQ==",
+    twitterLink: "https://x.com/kaushalendra03?t=I4ZnTBLIpvtT4VM14XedvQ&s=09",
+    linkedin_Link:
+      "https://www.linkedin.com/in/kaushalendrasingh03?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink:
+      "team/2023-27/kaushhalendra_247-20240406-0001 - 23IT015_Kaushalendra Singh.jpg",
+  },
+
+  {
+    nameID: "SatyamGupta",
+    email: "satyamstark7@gmail.com",
+    name: "Satyam Gupta",
+    branch: "Computer Science Engineering",
+    instagram_Link:
+      "https://www.instagram.com/satyam._.gupta._?igsh=MWZhdXhidnk3cmFqcw%3D%3D&utm_source=qr",
+    twitterLink: "#",
+    linkedin_Link: "https://in.linkedin.com/in/satyam-gupta-2b10742a4",
+    imageLink: "team/2023-27/IMG_0097 - Satyam Gupta.jpeg",
+  },
+
+  {
+    nameID: "BodhRatan",
+    email: "bodhratan1211@gmail.com",
+    name: "Bodh Ratan ",
+    branch: "Computer Science Engineering (AI & ML)",
+    instagram_Link: "https://www.instagram.com/_bodh._?igsh=cWVlZXRqNTYxbDdh",
+    twitterLink: "#",
+    linkedin_Link:
+      "https://www.linkedin.com/in/bodh-ratan-750a592b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink: "team/2023-27/bodhRatan.jpg",
+  },
+  {
+    nameID: "HarshKumarSingh",
+    email: "hkumarsingh137@gmail.com",
+    name: "Harsh Kumar Singh ",
+    branch: "Electrical and Electronics Engineering",
+    instagram_Link:
+      "https://www.instagram.com/harshkumar6383?utm_source=qr&igsh=aW12ZXIzcXgxNzU5",
+    twitterLink: "https://x.com/HarshKu77903621?t=MjtixHbDeVK_tfALGQnHmQ&s=08",
+    linkedin_Link:
+      "https://www.linkedin.com/in/harsh-kumar-singh-684831300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink:
+      "team/2023-27/Screenshot_2024-04-01-12-34-34-45_92460851df6f172a4592fca41cc2d2e6 - Harsh Kumar Singh.jpg",
+  },
   {
     nameID: "NazahatAli",
     email: "alinazahatkhan428@gmail.com",
@@ -282,6 +273,18 @@ const data = [
     twitterLink: "#",
     linkedin_Link: "#",
     imageLink: "team/2023-27/white-nazhat.jpg",
+  },
+  {
+    nameID: "KhushiTiwari",
+    email: "khushi.drdz@gmail.com",
+    name: "Khushi Tiwari",
+    branch: "Computer Science Engineering (AI & ML)",
+    instagram_Link:
+      "https://www.instagram.com/_kiziee.dz_?igsh=cXQ2eWNmNnozYmk1",
+    twitterLink: "#",
+    linkedin_Link:
+      "https://www.linkedin.com/in/khushi-tiwari-5012412a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    imageLink: "team/2023-27/20240407_180424-1 - Pramod Kumar Tiwari.jpg",
   },
 ];
 const membersOf2027 = {};
